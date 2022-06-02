62e71a38e54615f9cca701ac4061233e2d334779
Mohammad Sahil <sahilmejakhas@gmail.com>
2022-06-02 21:42:07 +0530

    component linked

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,7 @@
 import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
+import MissionVision from '../components/MissionVision'
 
 export default function Home() {
   return (
@@ -11,7 +12,7 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-     
+     <MissionVision/>
 
      <div style={{height: "100vh", background: "black", color: "white", display: "flex", alignItems: "center", justifyContent: "center", fontSize: "64px", fontWeight: "900"}}>This is the Home page of Project TBS</div>
     </div>
