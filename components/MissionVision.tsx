f6bcced4bcce6d71a8a3a1329d9291c376f7f55e
Subham Das <subham0464@gmail.com>
2022-06-03 21:06:08 +0530

    Update MissionVision.tsx
    added a Card component that takes props, so that making any changes later become easier.

--- components/MissionVision.tsx
+++ components/MissionVision.tsx
@@ -3,20 +3,33 @@
 import React from 'react'
 import '../index.css';
 
+interface Props {
+    title: string,
+    text: string,
+}
+
+const Card = ({title,text}:Props)=>{
+  return(
+    <div className="mx-auto w-10/12 py-16 px-24 lg:w-2/5 shadow-sm  bg-neutral-800 text-gray-500  font-normal my-16">
+          <h1 className="text-center text-4xl font-semibold text-amber-600 pb-12">{title}</h1>
+          <p className="text-2xl">{text}</p>
+      </div>
+  )
+}
+
 const MissionVision = () => {
   return (
     <>
         <h1 className="text-6xl text-green-500 font-mono text-center lg:ml-20 lg:text-left">MISSION & VISION</h1>
         <div className=" w-full lg:flex lg:flex-row">
-            <div className="mx-auto w-10/12 py-16 px-24 lg:w-2/5  bg-white shadow-sm  bg-neutral-800 text-gray-500 font-normal my-16">
-                <h1 className="text-center text-4xl font-semibold text-amber-600 pb-12">Our Mission</h1>
-                <p className="text-2xl"> Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc, tincidunt dis a sed tortor tortor malesuada quis euismod. Aenean ullamcorper integer tempor placerat diam. Ultrices enim ullamcorper ut egestas. Eget sapien.</p>
-            </div>
-            
-            <div className="mx-auto w-10/12 py-16 px-24 lg:w-2/5  bg-white shadow-sm  bg-neutral-800 text-gray-500  font-normal my-16">
-                <h1 className="text-center text-4xl font-semibold text-amber-600 pb-12">Our Vision</h1>
-                <p className="text-2xl">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc, tincidunt dis a sed tortor tortor malesuada quis euismod. Aenean ullamcorper integer tempor placerat diam. Ultrices enim ullamcorper ut egestas. Eget sapien.</p>
-            </div>
+            <Card
+            title={"Our Mission"}
+            text={"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc, tincidunt dis a sed tortor tortor malesuada quis euismod. Aenean ullamcorper integer tempor placerat diam. Ultrices enim ullamcorper ut egestas. Eget sapien."}
+            />
+            <Card
+            title={"Our Vision"}
+            text={"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc, tincidunt dis a sed tortor tortor malesuada quis euismod. Aenean ullamcorper integer tempor placerat diam. Ultrices enim ullamcorper ut egestas. Eget sapien."}
+            />
             
         </div>
         
