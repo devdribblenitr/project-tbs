fd8f3575396d4fbac10ab66483025e694477350d
ArpitaMandoi <arpitamandoi2003@gmail.com>
2022-06-06 12:10:42 +0530

    Added 'OurReach' component

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,6 +2,7 @@ import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
 import MissionVision from '../components/MissionVision'
+import OurReach from '../components/our_reach'
 
 export default function Home() {
   return (
@@ -13,6 +14,7 @@ export default function Home() {
       </Head>
 
      <MissionVision/>
+     <OurReach/>
 
      <div style={{height: "100vh", background: "black", color: "white", display: "flex", alignItems: "center", justifyContent: "center", fontSize: "64px", fontWeight: "900"}}>This is the Home page of Project TBS</div>
     </div>
