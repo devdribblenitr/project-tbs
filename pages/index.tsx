949211080758d0ff4028a04a25d032a486cdfc28
Subham Das <subhamdassneha@gmail.com>
2022-06-10 18:56:06 +0530

    updated
    setup tailwind css

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,6 +3,7 @@ import Image from 'next/image'
 import styles from '../styles/Home.module.css'
 import MissionVision from '../components/MissionVision'
 
+
 export default function Home() {
   return (
     <div className={styles.container}>
@@ -12,7 +13,10 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
+
      <MissionVision/>
+      
+     
 
      <div style={{height: "100vh", background: "black", color: "white", display: "flex", alignItems: "center", justifyContent: "center", fontSize: "64px", fontWeight: "900"}}>This is the Home page of Project TBS</div>
     </div>
