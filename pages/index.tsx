19fcd0c252bd83d88768c8a97748784d60d65786
Mohammad Sahil <sahilmejakhas@gmail.com>
2022-06-17 12:39:45 +0530

    navbar added

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,6 +3,7 @@ import Image from 'next/image'
 import styles from '../styles/Home.module.css'
 import MissionVision from '../components/MissionVision'
 import OurReach from '../components/our_reach'
+import Navbar from '../components/Navbar'
 
 export default function Home() {
   return (
@@ -12,11 +13,11 @@ export default function Home() {
         <meta name="description" content="The Bioscience Society - NIT Rourkela" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-
+      <Navbar/>
      <MissionVision/>
      <OurReach/>
 
-     <div style={{height: "100vh", background: "black", color: "white", display: "flex", alignItems: "center", justifyContent: "center", fontSize: "64px", fontWeight: "900"}}>This is the Home page of Project TBS</div>
+     {/* <div style={{height: "100vh", background: "black", color: "white", display: "flex", alignItems: "center", justifyContent: "center", fontSize: "64px", fontWeight: "900"}}>This is the Home page of Project TBS</div> */}
     </div>
   )
 }
