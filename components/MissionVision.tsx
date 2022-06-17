c3515a3179b05630bd8c4b715e750c75fcb80be9
Subham Das <subhamdassneha@gmail.com>
2022-06-17 13:25:20 +0530

    MissionVision Component

--- components/MissionVision.tsx
+++ components/MissionVision.tsx
@@ -1,23 +1,40 @@
 
 
+
+import React from 'react'
+
+
+interface Props {
+    title: string,
+    text: string,
+}
+
+const Card = ({title,text}:Props)=>{
+  return(
+    <div className="mx-auto w-10/12 py-16 px-24 lg:w-2/5 shadow-sm  bg-neutral-800 text-gray-500  font-normal my-16" >
+          <h1 className="text-center text-4xl font-semibold text-amber-600 pb-12" style={{color:'#DA8B7B'}}>{title}</h1>
+          <p className="text-2xl"  >{text}</p>
+      </div>
+  )
+}
+
 const MissionVision = () => {
   return (
-    <>
-        <h1 className="text-6xl text-green-500 font-mono text-center lg:ml-20 lg:text-left">MISSION & VISION</h1>
+    <div style={{fontFamily:'Montserrat', backgroundColor: '#181818'}} >
+        <h1 className="text-5xl text-center lg:ml-20 lg:text-left" style={{color:'#7FC43F'}}><b>Mission & Vision</b></h1>
         <div className=" w-full lg:flex lg:flex-row">
-            <div className="mx-auto w-10/12 py-16 px-24 lg:w-2/5  bg-white shadow-sm  bg-neutral-800 text-gray-500 font-normal my-16">
-                <h1 className="text-center text-4xl font-semibold text-amber-600 pb-12">Our Mission</h1>
-                <p className="text-2xl"> Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc, tincidunt dis a sed tortor tortor malesuada quis euismod. Aenean ullamcorper integer tempor placerat diam. Ultrices enim ullamcorper ut egestas. Eget sapien.</p>
-            </div>
-            
-            <div className="mx-auto w-10/12 py-16 px-24 lg:w-2/5  bg-white shadow-sm  bg-neutral-800 text-gray-500  font-normal my-16">
-                <h1 className="text-center text-4xl font-semibold text-amber-600 pb-12">Our Vision</h1>
-                <p className="text-2xl">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc, tincidunt dis a sed tortor tortor malesuada quis euismod. Aenean ullamcorper integer tempor placerat diam. Ultrices enim ullamcorper ut egestas. Eget sapien.</p>
-            </div>
+            <Card
+            title={"Our Mission"}
+            text={"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc, tincidunt dis a sed tortor tortor malesuada quis euismod. Aenean ullamcorper integer tempor placerat diam. Ultrices enim ullamcorper ut egestas. Eget sapien."}
+            />
+            <Card
+            title={"Our Vision"}
+            text={"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc, tincidunt dis a sed tortor tortor malesuada quis euismod. Aenean ullamcorper integer tempor placerat diam. Ultrices enim ullamcorper ut egestas. Eget sapien."}
+            />
             
         </div>
         
-    </>
+    </div>
   )
 }
 
